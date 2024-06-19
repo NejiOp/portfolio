2f9cfdde78eff3ef6128fd084c6f5c4bc50499fb
NejiOp <anuragmishra2102@gmail.com>
2024-06-20 00:08:55 +0530

    commit

--- Portfolio/mail.php
+++ Portfolio/mail.php
@@ -7,14 +7,14 @@ $Subject = $_POST['Subject'];
 $message = $_POST['message'];
 $to = "anuragmishra2102@gmail.com";
 $subject = "Mail From portfolio website";
-$txt = "Name = " . $name . "\r\nNumber = " . $phone . "\r\nEmail = " . $email . "\r\nEvent = " . $event . "\r\nMessage =" . $message;
-$headers = "From: $email" . "\r\n" .
-    "CC: anuragmishra2102@gmail.com";
+$txt = "Name = " . $name . "\r\nNumber = " . $phone . "\r\nEmail = " . $email . "\r\nMessage =" . $message;
+$headers = "From: $email" . "\r\n";
+
 if ($email != NULL) {
     mail($to, $subject, $txt, $headers);
 }
 
 // Thank you message
 $message = "Thank you for your submission! We will get back to you shortly.";
-echo "<script>alert('$message'); window.location.href = 'contact.html';</script>";
+echo "<script>alert('$message'); window.location.href = 'index.html';</script>";
 ?>
